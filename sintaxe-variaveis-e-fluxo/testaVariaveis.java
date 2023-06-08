64a7b5333bb0d5a6d3860460b4a22e85dbe3103a
Elson Felipe de Oliveira Urbano <elson_felipe@hotmail.com>
2023-06-08 11:27:30 -0300

    concluidas as aulas 3 e 4

---
+++ sintaxe-variaveis-e-fluxo/testaVariaveis.java
@@ -0,0 +1,23 @@
+public class testaVariaveis {
+
+    public static void main(String[] args){
+        System.out.println("ola novo teste");
+
+        int idade;
+        idade = 29;
+
+        System.out.println(idade);
+
+        idade = 30 + 10;
+
+        System.out.println(idade);
+
+        idade = 7 * 5 + 2;
+
+        System.out.println(idade);
+
+        System.out.println("a idade é " + idade + ", parabéns!");
+
+    }
+    
+}
