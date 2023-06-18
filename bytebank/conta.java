4b4fd4176bd64e98770b66b17d16a0de3b70f68e
Elson Felipe de Oliveira Urbano <elson_felipe@hotmail.com>
2023-06-18 11:59:24 -0300

    concluida a aula 3

--- bytebank/conta.java
+++ bytebank/conta.java
@@ -3,5 +3,30 @@ public class conta {
     int agencia;
     int numero;
     String titular;
+
+    public void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfere(double valor, conta destino) {
+        if (this.saldo >= valor) {
+            this.saca(valor);
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+
+    }
+
 }
+
     
---
+++ bytebank/testaMetodo.java
@@ -0,0 +1,23 @@
+public class testaMetodo {
+    public static void main(String[] args) {
+        conta contaDoPaulo = new conta();
+        contaDoPaulo.saldo = (100);
+        contaDoPaulo.deposita(50);
+        System.out.println(contaDoPaulo.saldo);
+
+        boolean consegiuRetirar = contaDoPaulo.saca(20);
+        System.out.println(contaDoPaulo.saldo);
+        System.out.println(consegiuRetirar);
+
+        conta contaDaMarcela = new conta();
+        contaDaMarcela.deposita(1000);
+
+        if(contaDaMarcela.transfere(300, contaDoPaulo)) {
+            System.out.println("Transferencia com sucesso");
+        } else {
+            System.out.println("Faltou dinheiro");
+        }
+        System.out.println(contaDaMarcela.saldo);
+        System.out.println(contaDoPaulo.saldo);
+    }
+}
