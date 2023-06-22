406c1390526e866d29b4002776734484a946cdff
Elson Felipe de Oliveira Urbano <elson_felipe@hotmail.com>
2023-06-22 12:34:31 -0300

    concluida a aula 2

--- bytebank-herdado/testeGerente.java
+++ bytebank-herdado/testeGerente.java
@@ -12,7 +12,10 @@ public class testeGerente {
 
         g1.setSenha(2222);
         boolean autenticou = g1.autentica(2222);
+
         System.out.println(autenticou);
+
+        System.out.println(g1.getBonificacao());
     }
     
 }
